cfcfa5b9a76ed9ae6936c0489350efbc97b8ab50
franciscoAbreuTecnico <francisco.abreu@tecnico.ulisboa.pt>
2025-01-29 15:57:25 +0000

    deleted out

--- out/_next/static/chunks/pages/garage-c6c73f9806d608b3.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[959],{8551:(_,n,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/garage",function(){return e(5110)}])},5110:(_,n,e)=>{"use strict";e.r(n),e.d(n,{default:()=>u});var r=e(4848);function u(){return(0,r.jsx)(r.Fragment,{})}e(9965),e(3471)},3471:()=>{}},_=>{var n=n=>_(_.s=n);_.O(0,[965,636,593,792],()=>n(8551)),_N_E=_.O()}]);
