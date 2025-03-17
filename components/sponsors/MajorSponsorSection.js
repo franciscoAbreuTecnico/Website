c840203ac85f94d35565250dfc7af0f25136c18c
franciscoAbreuTecnico <francisco.abreu@tecnico.ulisboa.pt>
2025-03-17 10:33:49 +0000

    Sponsors and Shop done. TODO news

--- components/sponsors/MajorSponsorSection.js
+++ components/sponsors/MajorSponsorSection.js
@@ -2,10 +2,13 @@ import styles from "../../styles/MajorSponsorSection.module.scss";
 
 export const headlineText = (
     <p className={styles.headline}>
-        We extend our deepest gratitude to all our sponsors for their invaluable support. 
-        Your generosity empowers us to reach new heights and achieve remarkable goals together. 
-        If you're interested in becoming a sponsor and helping our project thrive, feel free to send us a message. 
-        Together, we can make a meaningful impact!
+        We want to thank all our sponsors for their invaluable support.
+    </p>
+);
+
+export const secHeadlineText = (
+    <p className={styles.secHeadline}>
+        Your generosity and commitment to our vision are truly appreciated.
     </p>
 );
 
@@ -13,21 +16,26 @@ export default function MajorSponsorSection() {
     return (
         <div className={styles.container}>
             {headlineText}
-            <section className={styles.majorSponsor}>
-                <div className={styles.logoContainer}>
-                    <img src="/images/sponsors/smart_energy.png" alt="Major Sponsor Logo" />
-                </div>
-                <div className={styles.logoText}>
-                    <p>
-                        Thank you, <strong>SmartEnergy</strong>, for your unwavering support and generosity.
-                        Your partnership plays a pivotal role in driving our vision forward, empowering us to
-                        reach new heights and positively impact our community. We are deeply grateful for your 
-                        commitment and shared passion for innovation and growth. Together, we will continue to 
-                        create meaningful change. Your contribution is truly invaluable, and we look forward 
-                        to achieving even greater success together!
-                    </p>
-                </div>
-            </section>
+            {secHeadlineText}
         </div>
     );
 }
+
+// <div className={styles.container}>
+//     {headlineText}
+//     <section className={styles.majorSponsor}>
+//         <div className={styles.logoContainer}>
+//             <img src="/images/sponsors/smart_energy.png" alt="Major Sponsor Logo" />
+//         </div>
+//         <div className={styles.logoText}>
+//             <p>
+//                 Thank you, <strong>SmartEnergy</strong>, for your unwavering support and generosity.
+//                 Your partnership plays a pivotal role in driving our vision forward, empowering us to
+//                 reach new heights and positively impact our community. We are deeply grateful for your 
+//                 commitment and shared passion for innovation and growth. Together, we will continue to 
+//                 create meaningful change. Your contribution is truly invaluable, and we look forward 
+//                 to achieving even greater success together!
+//             </p>
+//         </div>
+//     </section>
+// </div>
