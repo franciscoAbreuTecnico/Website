f389ecf081646ad1325ffd38d2b0317fc41af16d
Jorge Santos <jorge.m.santos@tecnico.ulisboa.pt>
2025-07-12 11:56:15 +0100

    Added black page to garage

--- pages/garage/index.js
+++ pages/garage/index.js
@@ -1,5 +1,8 @@
 import MyGarageCards from '@/components/garage/GarageCards';
 
 export default function Garage() {
-  return <MyGarageCards />;
+  <div className="bg-black text-white min-h-screen flex items-center justify-center">
+    <h1 className="text-3xl font-bold">Black Page</h1>
+  </div>
+  /* return <MyGarageCards />; */
 }
