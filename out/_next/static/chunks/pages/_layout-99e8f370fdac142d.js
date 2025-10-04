4d0875dc31c2272ee49a3015a8d6e641117fdb4e
franciscoAbreuTecnico <francisco.abreu@tecnico.ulisboa.pt>
2025-10-04 14:52:21 +0100

    Delete out and add to gitignore file

--- out/_next/static/chunks/pages/_layout-99e8f370fdac142d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[77],{78188:(_,n,u)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_layout",function(){return u(26550)}])}},_=>{_.O(0,[636,593,792],()=>_(_.s=78188)),_N_E=_.O()}]);
